1f95f38e508a3003b20639a96c6b30edbc36836b
najwanurasyifa <najwanurasyifa286>
2026-04-22 14:11:32 +0800

    Kode Baru

--- KLP2.cpp
+++ KLP2.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    cout << "Welcome to KLP2! FADIL" << endl; 
+    cout << "Welcome to KLP2! FADIL, SYIFA" << endl; 
     return 0;
 }
