960782ab228713d9dce3ce1d243a17d535fd6276
nfjrh <nurulfajrih901@gmail.com>
2026-05-06 09:03:13 +0800

    Update di library.cpp

---
+++ library.cpp
@@ -0,0 +1,21 @@
+#include "library.h"
+#include <iomanip>
+
+// ===== SEKOLAH =====
+string lokasi[45] = {
+"SDN Inpres Perintis Kemerdekaan", "SMPN 30 Makassar", "SMAN 21 Makassar",
+"SD Inpres Karampuang 1", "SMP Kartika XX-1", "SMA Kartika XX-1",
+"SD Inpres Pettarani", "SMP Muhammadiyah 12", "SMKN 3 Makassar",
+"SD Inpres Pa'baeng-baeng", "SMP Muhammadiyah 1", "SMAN 11 Makassar",
+"SDN Nusantara", "SMP Wahyu Makassar", "SMK Dharma Nusantara",
+"SD Katolik Karya", "SMP Frater", "SMA Katolik Rajawali",
+"SDN Sudirman 1", "SMPN 2 Makassar", "SMAN 1 Makassar",
+"SD Inpres Maradekaya", "SMP Nasional Makassar", "SMA Nasional Makassar",
+"SD Inpres Mamajang 1", "SMP Hang Tuah", "SMA Kristen Makassar",
+"SDIT Al-Fityan", "SMPN 13 Makassar", "SMAN 9 Makassar",
+"SD Islam Cikal Harapan", "SMP Al-Azhar 21", "SMA Al-Azhar 12",
+"SD Katolik Mamajang", "SMPN 3 Makassar", "SMAN 14 Makassar",
+"SDN Antang 1", "SMPN 19 Makassar", "SMAN 10 Makassar",
+"SDN Borong", "SMPN 17 Makassar", "SMA LPP UMI",
+"SDN Malengkeri 1", "SMPN 18 Makassar", "SMAN 4 Makassar"
+};
