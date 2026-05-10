442d14e1067ceb113de593ebfc1ca2684235dccf
najwanurasyifa <najwanurasyifa286>
2026-05-10 15:39:54 +0800

    Buku

--- library.h
+++ library.h
@@ -2,23 +2,7 @@
 using namespace std;
 
 // ===== BUKU =====
-struct Buku {
-    string judul, penulis;
-    int tahun;
-    int stok;
-    int s;
+struct Buku{
+    string judul,penulis;
+    int tahun,stok;
 };
-
-// ===== ARRAY =====
-extern vector<Buku> daftarBuku;
-extern string lokasi[45];
-
-// ===== LINKED LIST (RIWAYAT) =====
-struct Node {
-    string nama, judul, sekolah;
-    bool kembali; // false = belum, true = sudah
-    Node* next;
-};
-
-void tambahRiwayat(string, string, string);
-void tampilRiwayat();
